0b74c8aefd37138fba4a04858d4ff37eb7b0473b
freejoe76 <joe.murphy@gmail.com>
2014-09-05 13:05:26 -0600

    Best.Commit.Ever.

--- update.py
+++ update.py
@@ -28,3 +28,16 @@ if __name__ == '__main__':
     }
     spready = Spready(**config)
     rows = spready.get_rows(options, 'responses')
+    # So. The production server runs on CentOs 5, which is the root of evil.
+    # Running a modern version of Python on CentOs 5 was doable, installing the
+    # necessary dependencies was not.
+    # We've got three things we need to do with the spreadsheet data:
+    #    1. Check if the card is in the database
+    #    2. If it's not, add the card to the db
+    #    3. If it's not, write the card javascript file and FTP it to extras
+    # So to do that, we're going to write the spreadsheet data to a flatfile,
+    # ingest that flatfile with PHP and do all the database / FTP work with PHP.
+    #
+    # Sigh.
+    for row in rows:
+        pass
